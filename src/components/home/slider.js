916d62b64045b9ac6995288cd573250b686355b1
Shane Wilson <shanewilsonpro@gmail.com>
2023-10-31 12:12:46 -0700

    strapi backend setup and using with axios for sliders

--- src/components/home/slider.js
+++ src/components/home/slider.js
@@ -2,24 +2,24 @@ import { View, Image, FlatList, Dimensions } from "react-native";
 
 import { styles } from "./styles/slider.styles";
 import { SPACING } from "../../theme/theme";
+import globalApi from "../../services/global-api";
+import { useEffect, useState } from "react";
 
 const { width } = Dimensions.get("window");
 
 export default function Slider() {
-  const sliderList = [
-    {
-      id: 1,
-      name: "Slider 1",
-      imageUrl:
-        "https://img.freepik.com/free-vector/team-doctors-medical-booking-app_23-2148552962.jpg?w=1380&t=st=1698601889~exp=1698602489~hmac=5e6a0f6846c7145697b955fe10565dd313827a35d73ce1cc0cbe0b68ba09d91d",
-    },
-    {
-      id: 2,
-      name: "slider 2",
-      imageUrl:
-        "https://img.freepik.com/premium-vector/doctors-provide-online-advice-patients-coronavirus-outbreak-using-video-calling_1150-43292.jpg?w=2000",
-    },
-  ];
+  const [sliderList, setSliderList] = useState([]);
+
+  useEffect(() => {
+    getSlider();
+  }, []);
+
+  const getSlider = () => {
+    globalApi.getSlider().then((resp) => {
+      setSliderList(resp.data.data);
+    });
+  };
+
   return (
     <View style={styles.Container}>
       <FlatList
@@ -32,7 +32,10 @@ export default function Slider() {
         decelerationRate={0}
         showsHorizontalScrollIndicator={false}
         renderItem={({ item }) => (
-          <Image source={{ uri: item.imageUrl }} style={styles.Image} />
+          <Image
+            source={{ uri: item.attributes.Image.data.attributes.url }}
+            style={styles.Image}
+          />
         )}
       />
     </View>
---
+++ src/services/global-api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = `http://${process.env.EXPO_PUBLIC_BASE_URL}:1337/api`;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: "Bearer " + process.env.EXPO_PUBLIC_STRAPI_API_KEY,
+  },
+});
+
+const getSlider = () => axiosInstance.get("/sliders?populate=*");
+
+export default { getSlider };
